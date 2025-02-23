a99c0630735b6e4a7314d1d5b81c47273e02695c
Tobias Niggemeyer <tobiasniggemeyer@googlemail.com>
2025-02-23 12:11:26 +0100

    add script to generate pubspec input for sound files

---
+++ tools/generate_assets.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main() {
+  String assetPath = 'assets/sounds/bullshit/';
+  Directory dir = Directory(assetPath);
+  
+  if (!dir.existsSync()) {
+    print("Der Ordner $assetPath existiert nicht.");
+    return;
+  }
+
+  List<String> files = dir
+      .listSync()
+      .whereType<File>()
+      .map((file) => file.path.replaceAll('\\', '/')) // Pfad anpassen für pubspec
+      .toList();
+
+  print("Kopiere die folgenden Zeilen in die pubspec.yaml:");
+  for (var file in files) {
+    print("    - $file");
+  }
+}
