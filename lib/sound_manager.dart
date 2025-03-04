d077cbf26b402c364ad042b5a9494b599b9260ea
Tobias Niggemeyer <tobiasniggemeyer@googlemail.com>
2025-03-04 20:45:08 +0100

    integrate android configuration, use fadeOut and fadeIn functions in playSound function

--- lib/sound_manager.dart
+++ lib/sound_manager.dart
@@ -6,16 +6,17 @@ class SoundManager {
   SoundManager();
 
   final AudioPlayer _audioPlayer = AudioPlayer();
-  static const _audioControlChannel = MethodChannel('com.example.audio_control');
+  static const _audioControlChannel = MethodChannel(
+    'com.example.audio_control',
+  );
 
   void initState() {
     _audioPlayer.onPlayerComplete.listen((event) {
       debugPrint("Audio playback complete. Now deactivating audio session.");
       _deactivateAudioSession();
     });
-    _configureIosAudioSession();
+    _configureAudioSession();
   }
-
 
   // Calls Swift code to setActive(false).
   Future<void> _deactivateAudioSession() async {
@@ -24,9 +25,9 @@ class SoundManager {
     } catch (e) {
       debugPrint('Error calling deactivateAudioSession: $e');
     }
-  }  
+  }
 
-  Future<void> _configureIosAudioSession() async {
+  Future<void> _configureAudioSession() async {
     await _audioPlayer.setAudioContext(
       AudioContext(
         iOS: AudioContextIOS(
@@ -36,6 +37,13 @@ class SoundManager {
             AVAudioSessionOptions.duckOthers,
           },
         ),
+        android: AudioContextAndroid(
+          isSpeakerphoneOn: true,
+          stayAwake: true,
+          contentType: AndroidContentType.music,
+          usageType: AndroidUsageType.media,
+          audioFocus: AndroidAudioFocus.gainTransientMayDuck,
+        ),
       ),
     );
   }
@@ -44,10 +52,29 @@ class SoundManager {
     // beep.mp3 must be declared in pubspec.yaml under assets:
     // assets/sounds/beep.mp3
     try {
-        await _audioPlayer.play(AssetSource(filename));
-      } catch (e) {
-        print(e);
-      }
+      await fadeOutBackgroundMusic();
+      await _audioPlayer.play(AssetSource(filename));
+      await fadeInBackgroundMusic();
+    } catch (e) {
+      print(e);
+    }
   }
 
+  Future<void> fadeOutBackgroundMusic() async {
+    const int steps = 10;
+    const Duration stepDuration = Duration(milliseconds: 100);
+    for (int i = 0; i < steps; i++) {
+      await _audioPlayer.setVolume(1.0 - (i + 1) / steps);
+      await Future.delayed(stepDuration);
+    }
+  }
+
+  Future<void> fadeInBackgroundMusic() async {
+    const int steps = 10;
+    const Duration stepDuration = Duration(milliseconds: 100);
+    for (int i = 0; i < steps; i++) {
+      await _audioPlayer.setVolume((i + 1) / steps);
+      await Future.delayed(stepDuration);
+    }
+  }
 }
